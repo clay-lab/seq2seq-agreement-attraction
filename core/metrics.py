5024c1e585393ec7da2e0e685a7a09e91cc28a54
mawilson1234 <michael.a.wilson@yale.edu>
2022-09-29 12:50:20 -0400

    debug

--- core/metrics.py
+++ core/metrics.py
@@ -539,6 +539,7 @@ def agreement_attraction_any(
 	'''Is there agreement attraction with the closest preceding distractor?'''
 	# attraction doesn't mean anything if there's no possible evidence for it,
 	# so return None
+	breakpoint()
 	if trn_lang in ['en_RC_PP', 'en_RC_PP_gen'] and tense == 'past':
 		return None
 	
