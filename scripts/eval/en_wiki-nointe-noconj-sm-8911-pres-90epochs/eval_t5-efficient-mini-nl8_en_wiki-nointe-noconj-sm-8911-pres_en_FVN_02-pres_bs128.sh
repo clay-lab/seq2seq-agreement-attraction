b3acecbed79d67bf877414995e37358d08ca7311
mawilson1234 <michael.a.wilson@yale.edu>
2022-11-30 16:57:57 -0500

    update scripts'

---
+++ scripts/eval/en_wiki-nointe-noconj-sm-8911-pres-90epochs/eval_t5-efficient-mini-nl8_en_wiki-nointe-noconj-sm-8911-pres_en_FVN_02-pres_bs128.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#SBATCH --job-name=t5-efficient-mini-nl8-eval-tense-en_wiki-nointe-noconj-sm-8911-pres
+#SBATCH --output=joblogs/%x_%j.txt
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=30GB
+#SBATCH --time=10:00:00
+#SBATCH --gpus=v100:1
+#SBATCH --partition=gpu
+#SBATCH --mail-type=END,FAIL,INVALID_DEPEND
+
+module load CUDA
+module load cuDNN
+module load miniconda
+
+source activate /gpfs/gibbs/project/frank/ref4/conda_envs/py38-agratt
+
+python core/run_seq2seq.py \
+	--model_name_or_path 'google/t5-efficient-mini-nl8' \
+	--do_learning_curve \
+	--task translation_src_to_tgt \
+	--train_file data/en_wiki-nointe-noconj-sm-8911-pres/en_wiki-nointe-noconj-sm-8911-pres_train.json.gz \
+	--validation_file data/en_FVN_02-pres/en_FVN_02-pres_test.json.gz \
+	--output_dir outputs/en_wiki-nointe-noconj-sm-8911-pres-30epochs/t5-efficient-mini-nl8-finetuning-en_wiki-nointe-noconj-sm-8911-pres-bs128/ \
+	--per_device_train_batch_size=8 \
+	--per_device_eval_batch_size=16 \
+	--overwrite_output_dir \
+	--predict_with_generate \
+	--num_train_epochs 30.0
