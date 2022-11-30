6aad42f8d01bab0e39f8646755b35d92c4158e07
Michael Wilson <michael.a.wilson@yale.edu>
2022-11-30 16:13:01 -0500

    permissions

---
+++ scripts/eval/sub_scripts/sub_scripts_tmp/base_el_dl_nl/eval_t5-base-nl2-60k_wiki_en_RC_PP-dist-pres_bs128.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#SBATCH --job-name=t5-base-nl2-60k-eval
+#SBATCH --output=joblogs/%x_%j.txt
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=20GB
+#SBATCH --time=10:00:00
+#SBATCH --gpus=v100:1
+#SBATCH --partition=gpu
+#SBATCH --mail-type=END,FAIL,INVALID_DEPEND
+
+module load CUDA
+module load cuDNN
+module load miniconda
+
+source activate /gpfs/gibbs/project/frank/ref4/conda_envs/py38-agratt
+
+python core/run_seq2seq.py \
+	--model_name_or_path 'google/t5-efficient-base-nl2' \
+	--do_learning_curve \
+	--task translation_src_to_tgt \
+	--train_file data/en_RC_PP-nodist-pres-and-past/en_RC_PP-nodist-pres-and-past_train.json.gz \
+	--validation_file data/en_RC_PP-dist-pres/en_RC_PP-dist-pres_test.json.gz \
+	--output_dir outputs/t5-efficient-base-nl2-60k-finetuning-en_wiki-nodist-pres-and-past-bs128/ \
+	--per_device_train_batch_size=8 \
+	--per_device_eval_batch_size=16 \
+	--overwrite_output_dir \
+	--predict_with_generate \
+        
