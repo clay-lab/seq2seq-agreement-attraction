0080b85a753269bda81e7a6b7057649065acbee2
Michael Wilson <michael.a.wilson@yale.edu>
2022-12-13 15:48:33 -0500

    import Set

--- core/run_seq2seq.py
+++ core/run_seq2seq.py
@@ -34,7 +34,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
-from typing import Optional
+from typing import Optional, Set
 from metrics import compute_metrics as run_metrics # workaround since hf already defines a function with this name
 from operator import itemgetter
 from datasets import load_dataset, load_metric
