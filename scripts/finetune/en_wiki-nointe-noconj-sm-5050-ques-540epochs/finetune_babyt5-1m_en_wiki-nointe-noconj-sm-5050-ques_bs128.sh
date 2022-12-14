58a7e52a85155d0845ecbb91a24f701c8168de44
mawilson1234 <michael.a.wilson@yale.edu>
2022-12-14 17:25:38 -0500

    update scripts to match fine-tuning steps

---
+++ scripts/finetune/en_wiki-nointe-noconj-sm-5050-ques-540epochs/finetune_babyt5-1m_en_wiki-nointe-noconj-sm-5050-ques_bs128.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH --job-name=babyt5-1m-finetune-tense-en_wiki-nointe-noconj-sm-5050-ques
+#SBATCH --output=joblogs/%x_%j.txt
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=30GB
+#SBATCH --time=10:00:00
+#SBATCH --gpus=v100:1
+#SBATCH --partition=gpu
+#SBATCH --mail-type=END,FAIL,INVALID_DEPEND
+
+module load CUDA
+module load cuDNN
+module load miniconda
+
+source activate /gpfs/gibbs/project/frank/ref4/conda_envs/py38-agratt
+
+python core/run_seq2seq.py \
+	--model_name_or_path 'mueller-t5-checkpoints/babyt5-1m' \
+	--do_train \
+	--task translation_src_to_tgt \
+	--train_file data/en_wiki-nointe-noconj-sm-5050-ques/en_wiki-nointe-noconj-sm-5050-ques_train.json.gz \
+	--validation_file data/en_wiki-nointe-noconj-sm-5050-ques/en_wiki-nointe-noconj-sm-5050-ques_dev.json.gz \
+	--output_dir outputs/en_wiki-nointe-noconj-sm-5050-ques-540epochs/babyt5-1m-finetuning-en_wiki-nointe-noconj-sm-5050-ques-bs128/ \
+	--per_device_train_batch_size=4 \
+	--gradient_accumulation_steps=32 \
+	--per_device_eval_batch_size=16 \
+	--overwrite_output_dir \
+	--predict_with_generate \
+	--num_train_epochs 540.0
