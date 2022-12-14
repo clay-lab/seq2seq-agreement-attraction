44b976861e69a341b98dea1a260c7e993f33dd99
mawilson1234 <michael.a.wilson@yale.edu>
2022-12-14 14:31:25 -0500

    add tuning scripts

---
+++ scripts/eval/en_wiki-nointe-noconj-sm-5050-ques-and-past-270epochs/eval_t5-efficient-base-nl2_en_wiki-nointe-noconj-sm-5050-ques-and-past_en_VN_98-ques_bs128.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#SBATCH --job-name=t5-efficient-base-nl2-eval-tense-en_wiki-nointe-noconj-sm-5050-ques-and-past
+#SBATCH --output=joblogs/%x_%j.txt
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=30GB
+#SBATCH --time=10:00:00
+#SBATCH --gpus=v100:1
+#SBATCH --partition=gpu
+#SBATCH --mail-type=END,FAIL,INVALID_DEPEND
+
+module load CUDA
+module load cuDNN
+module load miniconda
+
+source activate /gpfs/gibbs/project/frank/ref4/conda_envs/py38-agratt
+
+python core/run_seq2seq.py \
+	--model_name_or_path 'google/t5-efficient-base-nl2' \
+	--do_learning_curve \
+	--task translation_src_to_tgt \
+	--train_file data/en_wiki-nointe-noconj-sm-5050-ques-and-past/en_wiki-nointe-noconj-sm-5050-ques-and-past_train.json.gz \
+	--validation_file data/en_VN_98-ques/en_VN_98-ques_test.json.gz \
+	--output_dir outputs/en_wiki-nointe-noconj-sm-5050-ques-and-past-270epochs/t5-efficient-base-nl2-finetuning-en_wiki-nointe-noconj-sm-5050-ques-and-past-bs128/ \
+	--per_device_train_batch_size=8 \
+	--per_device_eval_batch_size=16 \
+	--overwrite_output_dir \
+	--predict_with_generate \
+	--num_train_epochs 270.0
---
+++ scripts/finetune/en_wiki-nointe-noconj-sm-5050-ques-270epochs/finetune_t5-efficient-mini-nl24_en_wiki-nointe-noconj-sm-5050-ques_bs128.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH --job-name=t5-efficient-mini-nl24-finetune-tense-en_wiki-nointe-noconj-sm-5050-ques
+#SBATCH --output=joblogs/%x_%j.txt
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=30GB
+#SBATCH --time=10:00:00
+#SBATCH --gpus=v100:1
+#SBATCH --partition=gpu
+#SBATCH --mail-type=END,FAIL,INVALID_DEPEND
+
+module load CUDA
+module load cuDNN
+module load miniconda
+
+source activate /gpfs/gibbs/project/frank/ref4/conda_envs/py38-agratt
+
+python core/run_seq2seq.py \
+	--model_name_or_path 'google/t5-efficient-mini-nl24' \
+	--do_train \
+	--task translation_src_to_tgt \
+	--train_file data/en_wiki-nointe-noconj-sm-5050-ques/en_wiki-nointe-noconj-sm-5050-ques_train.json.gz \
+	--validation_file data/en_wiki-nointe-noconj-sm-5050-ques/en_wiki-nointe-noconj-sm-5050-ques_dev.json.gz \
+	--output_dir outputs/en_wiki-nointe-noconj-sm-5050-ques-270epochs/t5-efficient-mini-nl24-finetuning-en_wiki-nointe-noconj-sm-5050-ques-bs128/ \
+	--per_device_train_batch_size=4 \
+	--gradient_accumulation_steps=32 \
+	--per_device_eval_batch_size=16 \
+	--overwrite_output_dir \
+	--predict_with_generate \
+	--num_train_epochs 270.0
