3b713fd31bc4757f110916f288dfa4b1bd3a6a3c
mawilson1234 <michael.a.wilson@yale.edu>
2023-02-24 12:22:34 -0500

    new scripts, dataset

---
+++ scripts/eval/en_wiki-nodist-noconj-sm-8911-pres-7812updates/eval_t5-efficient-base-dl2_en_wiki-nodist-noconj-sm-8911-pres_en_RC_PP-dist-pres-bala_bs128.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --job-name=t5-efficient-base-dl2-eval-tense-en_wiki-nodist-noconj-sm-8911-pres
+#SBATCH --output=joblogs/%x_%j.txt
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=32GB
+#SBATCH --time=02-00:00:00
+#SBATCH --gpus=v100:1
+#SBATCH --partition=gpu
+#SBATCH --mail-type=END,FAIL,INVALID_DEPEND
+
+module load CUDA
+module load cuDNN
+module load miniconda
+
+source activate /gpfs/gibbs/project/frank/ref4/conda_envs/py38-agratt
+
+python core/run_seq2seq.py \
+	--model_name_or_path 'google/t5-efficient-base-dl2' \
+	--do_learning_curve \
+	--validation_file data/en_RC_PP-dist-pres-bala/en_RC_PP-dist-pres-bala_test.json.gz \
+	--output_dir outputs/en_wiki-nodist-noconj-sm-8911-pres-7812updates/t5-efficient-base-dl2-finetuning-en_wiki-nodist-noconj-sm-8911-pres-bs128/ \
+	--per_device_eval_batch_size 16 \
+	--predict_with_generate \
+	--predict_from_given_words_after_identical \
+	--val_max_target_length 75
---
+++ scripts/finetune/en_wiki-nodist-noconj-sm-8911-pres-7812updates/finetune_simplewiki-100m_en_wiki-nodist-noconj-sm-8911-pres_bs128.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --job-name=simplewiki-100m-finetune-tense-en_wiki-nodist-noconj-sm-8911-pres
+#SBATCH --output=joblogs/%x_%j.txt
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=30GB
+#SBATCH --time=02-00:00:00
+#SBATCH --gpus=v100:1
+#SBATCH --partition=gpu
+#SBATCH --mail-type=END,FAIL,INVALID_DEPEND
+
+module load CUDA
+module load cuDNN
+module load miniconda
+
+source activate /gpfs/gibbs/project/frank/ref4/conda_envs/py38-agratt
+
+python core/run_seq2seq.py \
+	--model_name_or_path 'mueller-t5-checkpoints/simplewiki-100m' \
+	--do_train \
+	--train_file data/en_wiki-nodist-noconj-sm-8911-pres/en_wiki-nodist-noconj-sm-8911-pres_train.json.gz \
+	--output_dir outputs/en_wiki-nodist-noconj-sm-8911-pres-7812updates/simplewiki-100m-finetuning-en_wiki-nodist-noconj-sm-8911-pres-bs128/ \
+	--per_device_train_batch_size 4 \
+	--gradient_accumulation_steps 32 \
+	--overwrite_output_dir \
+	--max_steps 7812
